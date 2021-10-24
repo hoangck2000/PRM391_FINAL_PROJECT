134cf5449c6a500144a4bb68a2ae06176329d332
hoangck2000 <nguyenhoangck2000@gmail.com>
2021-10-24 19:18:06 +0700

    Create Welcome Screen

--- PRM391_FINAL_PROJECT/app/src/main/java/com/example/prm_final_project/WelcomeScreen.java
+++ PRM391_FINAL_PROJECT/app/src/main/java/com/example/prm_final_project/WelcomeScreen.java
@@ -2,13 +2,24 @@ package com.example.prm_final_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class WelcomeScreen extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(WelcomeScreen.this, HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        })
     }
 }
