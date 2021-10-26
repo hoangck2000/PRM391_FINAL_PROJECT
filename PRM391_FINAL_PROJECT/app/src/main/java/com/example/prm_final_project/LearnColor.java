923249e2f40f62e35b6ad373e8b94eb9dc763346
Duy Đồng <dongminhduy2000@gmail.com>
2021-10-26 20:37:22 +0700

    Duy1cm-re

---
+++ PRM391_FINAL_PROJECT/app/src/main/java/com/example/prm_final_project/LearnColor.java
@@ -0,0 +1,14 @@
+package com.example.prm_final_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LearnColor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_learn_color);
+    }
+}
