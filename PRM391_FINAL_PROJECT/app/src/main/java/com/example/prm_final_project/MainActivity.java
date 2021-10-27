bd596411172e9159c573ad842ff01c4c17aa55bd
hoangck2000 <nguyenhoangck2000@gmail.com>
2021-10-27 21:31:09 +0700

    Demo Free Draw Activity

--- PRM391_FINAL_PROJECT/app/src/main/java/com/example/prm_final_project/MainActivity.java
+++ PRM391_FINAL_PROJECT/app/src/main/java/com/example/prm_final_project/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.prm_final_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static int SPLASH_TIME_OUT = 1500;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void goToFreeDraw(View view) {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, FreeDrawActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
 }
