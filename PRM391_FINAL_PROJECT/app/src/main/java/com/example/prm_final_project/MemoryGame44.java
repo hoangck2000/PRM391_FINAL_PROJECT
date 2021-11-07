31fcc0a55d711b1365e70a96cc98303f5e09c77b
longnhhe141364@fpt.edu.vn <longnhhe141364@fpt.edu.vn>
2021-11-08 00:10:56 +0700

    first

--- PRM391_FINAL_PROJECT/app/src/main/java/com/example/prm_final_project/MemoryGame44.java
+++ PRM391_FINAL_PROJECT/app/src/main/java/com/example/prm_final_project/MemoryGame44.java
@@ -1,2 +1,13 @@
-package com.example.prm_final_project;public class MemoryGame44 {
+package com.example.prm_final_project;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MemoryGame44 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_memory_game_2);
+    }
 }
--- PRM391_FINAL_PROJECT/app/src/main/java/com/example/prm_final_project/MemoryGameMenu.java
+++ PRM391_FINAL_PROJECT/app/src/main/java/com/example/prm_final_project/MemoryGameMenu.java
@@ -1,13 +1,35 @@
 package com.example.prm_final_project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MemoryGame extends AppCompatActivity {
+public class MemoryGameMenu extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 1000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_memory_game_menu);
+    }
+    public void goTo3x3(View view) {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MemoryGameMenu.this, MemoryGame33.class);
+                startActivity(intent);
+            }
+        }, SPLASH_TIME_OUT);
+    }
+    public void goTo4x4(View view) {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MemoryGameMenu.this, MemoryGame44.class);
+                startActivity(intent);
+            }
+        }, SPLASH_TIME_OUT);
     }
 }
