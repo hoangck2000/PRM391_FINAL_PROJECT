f63ecef19c6e065d7329f9694ab5f736405c2fe2
NhungHong83 <nhungnthhe141425@fpt.edu.vn>
2021-11-09 09:13:06 +0700

    update quiz

---
+++ PRM391_FINAL_PROJECT/app/src/main/java/com/example/prm_final_project/QuizContent_Flower.java
@@ -0,0 +1,14 @@
+package com.example.prm_final_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QuizContent_Flower extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz_content_flower);
+    }
+}
---
+++ PRM391_FINAL_PROJECT/app/src/main/java/com/example/prm_final_project/Quizs/Question.java
@@ -0,0 +1,2 @@
+package com.example.prm_final_project.Quizs;public class Question {
+}
